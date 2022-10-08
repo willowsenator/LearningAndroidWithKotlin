5fea1db4b748b4466ac96c227520de4e90657022
willowsenator <willowsenator@gmail.com>
2022-10-09 00:17:45 +0200

    Activity Android

---
+++ app/src/main/java/com/willowsenator/learningandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.willowsenator.learningandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hola Toast", Toast.LENGTH_LONG).show()
+    }
+}
